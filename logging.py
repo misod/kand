357e483dd17492f46a9aea5757146356e70aff05
Joakim Ingnäs <joakimingnas@student247-48.eduroam.uu.se>
2017-04-13 13:54:47 +0200

    Only one file, fixed minor issues

--- logging.py
+++ logging.py
@@ -3,117 +3,103 @@ Param:
 logType, an int corresponding to which type of log, 0 is a regular log, 1 is a smaller error log and 2 is a major error log
 logMessage, which message to add to the errorlog
 Output:
-Void
+Returns 1 if successfull, otherwise 0
 Summary:
 Evaluates the sent logType, chooses a correct auxilliary function for each type of error.
 """
 def addLog(logType, logMessage):
+    val_return = 0
+
     if logType == 0:
-        if simpleLog(logMessage):
+
+        if Log(logMessage, simpleLog):
+            val_return = 1
             print("Message logged")
+
         else:
             print("Error: Problem logging the message")
+
     elif logType == 1:
-        if smallErrorLog(logMessage):
+
+        if Log(logMessage, smallErrorLog):
+            val_return = 1
             print("Message logged")
+
         else:
             print("Error: Problem logging the message")
+
     elif logType == 2:
-        if bigErrorLog(logMessage):
+
+        if Log(logMessage, bigErrorLog):
+            val_return = 1
             print("Message logged")
+
         else:
             print("Error: Problem logging the message")
+
     else:
         print("Error: Invalid log type")
 
+    return val_return
+
+
 """
 Param:
-logMessage, which message to add to the errorlog
+logMessage, which message to add to the log file
+logFile, which log file to add the message to
 Output:
 1 if message logged, 0 if log failed
 Summary:
 Attempts to write the given log message to the regular log file
 """
-def simpleLog(logMessage):
+def Log(logMessage, logFile):
     val_return = 0
 
-    if os.path.isfile(simpleLog) and os.access(simpleLog, os.W_OK):
-        with open(simpleLog, 'a') as log:
+    if os.path.isfile(logFile) and (os.access(logFile), os.W_OK):
+
+        with open(logFIle, 'a') as log:
+
             try:
                 log.write(logMessage)
                 val_return = 1
+
             except Exception:
                 print("Error writing to logfile")
+
             finally:
                 log.close()
                 return val_return
+
     else:
         print("Logfile doesn't exist")
         return val_return
 
 """
 Param:
-logMessage, which message to add to the errorlog
+logMessage, what to log
 Output:
-1 if message logged, 0 if log failed
+returns 1 if message logged, 0 if failed
 Summary:
-Attempts to write the given log message to the small error log file
+used to store FLARM data packets during development
 """
-def smallErrorLog(logMessage):
-    val_return = 0
-
-    if os.path.isfile(smallErrorLog) and os.access(smallErrorLog, os.W_OK):
-        with open(smallErrorLog, 'a') as log:
-            try:
-                log.write(logMessage)
-                val_return = 1
-            except Exception:
-                print("Error writing to logfile")
-            finally:
-                log.close()
-                return val_return
-    else:
-        print("Logfile doesn't exist")
-        return val_return
-
-"""
-Param:
-logMessage, which message to add to the errorlog
-Output:
-1 if message logged, 0 if log failed
-Summary:
-Attempts to write the given logmessage to the major error log file
-"""
-def bigErrorLog(logMessage):
-    val_return = 0
-
-    if os.path.isfile(bigErrorLog) and os.access(bigErrorLog, os.W_OK):
-        with open(bigErrorLog, 'a') as log:
-            try:
-                log.write(logMessage)
-                val_return = 1
-            except Exception:
-                print("Error writing to logfile")
-            finally:
-                log.close()
-                return val_return
-    else:
-        print("Logfile doesn't exist")
-        return val_return
-
 def logPacket(logMessage):
     val_return = 0
 
-if os.path.isfile(packetLog) and os.access(packetLog, os.W_OK):
-    with open(packetLog, 'a') as log:
-        try:
-            log.write(logMessage)
-            val_return = 1
-        except Exception:
-            print("Error writing to logfile")
-        finally:
-            log.close()
-            return val_return
-else:
-    print("Logfile doesn't exist")
-    return val_return
+    if os.path.isfile(packetLog) and os.access(packetLog, os.W_OK):
+
+        with open(packetLog, 'a') as log:
+
+            try:
+                log.write(logMessage)
+                val_return = 1
+
+            except Exception:
+                print("Error writing to logfile")
+
+            finally:
+                log.close()
+                return val_return
+
+    else:
+        print("Logfile doesn't exist")
+        return val_return
