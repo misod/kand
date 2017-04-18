8d3ac97d14a529726dfc7a475f24daeaaa6e52a2
Mikael söderberg <mikael.misod@gmail.com>
2017-04-18 14:10:25 +0200

    påbörjat funktiner

--- packets.py
+++ packets.py
@@ -3,6 +3,8 @@
 import logging
 import helpers
 
+threshold_speed = 4
+
 def relevant_package(array_whit_id, package):
     if package[0].src_callsign is not None and len(package[0].src_callsign) > 4:
         try:
@@ -16,10 +18,18 @@ def relevant_package(array_whit_id, package):
         return False
 
 def processing(package):
+
     #determin if plane is on ground
     #se which is the towing plane
     #log to database
     #starting flight or landing?
+    plane_stationary = plane_on_ground(package)
+
+    if active_flight(packets):
+        #do some more processing
+        # se if it's time re regiser ended flight
+    elif not plane_stationary:
+        # time to register a new_flight
 
     return True
 
@@ -28,7 +38,11 @@ def determine_towing_plane(package):
 
     return ""
 
+def active_flight(packet):
+
+    return True
+
 def plane_on_ground(package):
 
 
-    return ""
+    return True
