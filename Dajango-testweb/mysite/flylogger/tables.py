51d460d7f8c614d5cf3618151a37afec6fa7f332
Conrad Åslund <conradswe>
2017-05-04 14:17:47 +0200

    fungerar nu

--- Dajango-testweb/mysite/flylogger/tables.py
+++ Dajango-testweb/mysite/flylogger/tables.py
@@ -1,6 +1,11 @@
 import django_tables2 as tables
 from .models import Person
 
+class PersonTable(tables.Table):
+    class Meta:
+        model = Person
+        # add class="paleblue" to <table> tag
+        attrs = {'class': 'paleblue'}
 
 #class PersonTable(tables.Table):
 #    class Meta:
--- Dajango-testweb/mysite/flylogger/views.py
+++ Dajango-testweb/mysite/flylogger/views.py
@@ -1,9 +1,11 @@
-from flylogger import models
 from django.shortcuts import render
 from django_tables2 import RequestConfig
 from .models import Person
-#from .tables import PersonTable
+from .tables import PersonTable
 
 def people(request):
-    return render(request, 'people.html', {'people': Person.objects.all()})
+    table = PersonTable(Person.objects.all())
+    RequestConfig(request).configure(table)
+    return render(request, 'people.html', {'table': table})
+
 # Create your views here.
