c709f0cc5ddd70bdd5c635173a85f2343430cc6b
Conrad Åslund <conradswe>
2017-05-11 16:08:57 +0200

    borjan pa datepick

--- Dajango-testweb/mysite/flylogger/views.py
+++ Dajango-testweb/mysite/flylogger/views.py
@@ -5,9 +5,11 @@ from django_tables2 import RequestConfig
 from .models import (FlightData, Glider, TowPlane)
 from .tables import (LoggTable, GliderTable)
 from .admin import FlightDataResource
+import datetime
 
 def people(request):
-    table1 = LoggTable(FlightData.objects.all().select_related('glider'), order_by="-logged_date")
+    today = datetime.datetime.today()
+    table1 = LoggTable(FlightData.objects.filter(logged_date = today ).select_related('glider'), order_by="-logged_date")
     RequestConfig(request, paginate={'per_page': 20}).configure(table1)
     return render(request, 'people.html', {'table1': table1}, )
 
